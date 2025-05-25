42ab50b7e54689fe0b8a088dc142c7c6496c2559
PiotrKrosniak <PiotrKrosniak@users.noreply.github.com>
2025-05-25 08:35:46 +0000

    Deploying to gh-pages from @ PiotrKrosniak/BioresilienceTracker@4ce0fd57c54439317cfe0b160edb255442ea9ec1 🚀

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const config = {
-  GOOGLE_MAPS_API_KEY: '',
-  SPREADSHEET_ID: ''
+  GOOGLE_MAPS_API_KEY: 'AIzaSyBO_DjJX7kF9iocqkw_MZJ09nJw_3-IVSw',
+  SPREADSHEET_ID: '1m-X2TRl5MDlKUA2f5LqsjW0a98hFYujFg8mji-KZemo'
 };
