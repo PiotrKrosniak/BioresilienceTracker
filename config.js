09d46c9666362d27d1767d36603ea8f94992a6e8
PiotrKrosniak <p.krosniak@gmail.com>
2025-05-26 07:59:49 +0200

    update 1 file and delete 6 files

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY || 'YOUR_DEFAULT_KEY_HERE',
+    SPREADSHEET_ID: process.env.SPREADSHEET_ID || ''
+};
+
+export default config; 
